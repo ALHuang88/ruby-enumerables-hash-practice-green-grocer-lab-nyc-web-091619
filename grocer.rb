f7e4a50bfa8b0f375fb79d5fe46ba0edb69789f7
Allen Huang <alhuang1288@gmail.com>
2019-08-28 17:17:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,7 +14,11 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # code here
+  {
+  "AVOCADO" => {:price => 3.00, :clearance => true, :count => 1},
+  "KALE"    => {:price => 3.00, :clearance => false, :count => 1},
+  "AVOCADO W/COUPON" => {:price => 2.50, :clearance => true, :count => 2},
+}
 end
 
 def checkout(cart, coupons)
