93ab0041185cd7341934ae2f8b2de2a1674b9131
Allen Huang <alhuang1288@gmail.com>
2019-08-28 17:17:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -7,7 +7,10 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  {
+  "AVOCADO" => {:price => 3.00, :clearance => true, :count => 3},
+  "KALE"    => {:price => 3.00, :clearance => false, :count => 1}
+}
 end
 
 def apply_clearance(cart)
